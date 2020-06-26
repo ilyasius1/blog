ae94722ab7000b39d54695d81643de395e27c6fd
ilyasius1 <ilyasius@gmail.com>
2020-06-26 17:53:38 +0300

    two

---
+++ app/Includes/Classes/MyMenu.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Includes\Classes;
+
+
+use App\Includes\Interfaces\MyMenuInterface;
+
+class MyMenu implements MyMenuInterface
+{
+    public function one(){
+
+    }
+}
---
+++ app/Includes/Interfaces/MyMenuInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Includes\Interfaces;
+
+interface MyMenuInterface
+{
+    public function one();
+}
